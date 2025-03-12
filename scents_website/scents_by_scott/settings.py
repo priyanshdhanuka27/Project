f6a541135ee934d2c55b77a9f8f45c32b5bf1139
priyansh <priyanshdhanuka27@gmail.com>
2025-03-11 20:22:04 -0700

    changes to link css

--- scents_website/scents_by_scott/settings.py
+++ scents_website/scents_by_scott/settings.py
@@ -115,7 +115,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# this is included when I have a static folder outside the app directories
+import os
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
