a2444803ffda6d65bf99d86e4af1020a2232103b
priyansh <priyanshdhanuka27@gmail.com>
2025-03-11 20:11:26 -0700

    made views for django to read the webpages

--- scents_website/scents_by_scott/urls.py
+++ scents_website/scents_by_scott/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('index.urls')),
+    path('about/', include('about.urls')),
+    path('products/', include('products.urls')),
+    path('login/', include('client_login.urls')),
+    path('cart/', include('cart.urls')),
 ]
